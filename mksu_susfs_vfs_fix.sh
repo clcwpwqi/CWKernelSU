f5010628ca5e4abd1f272ac442530355f7028fa0
樱檩殇雪 <109797057+ShirkNeko@users.noreply.github.com>
2025-03-01 05:03:58 +0800

    Add files via upload

---
+++ mksu_susfs_vfs_fix.sh
@@ -0,0 +1,9 @@
+﻿(
+sed -i '/#include "selinux\/selinux.h"/a \
+bool ksu_vfs_read_hook = true;\n\
+bool ksu_execveat_hook = true;\n\
+bool ksu_input_hook = true;\n\
+EXPORT_SYMBOL(ksu_vfs_read_hook);\n\
+EXPORT_SYMBOL(ksu_execveat_hook);\n\
+EXPORT_SYMBOL(ksu_input_hook);' ./kernel/ksud.c
+)
