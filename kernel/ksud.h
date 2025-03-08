9d92e599af75b838911642b82fd098f6ddcee011
樱檩殇雪 <2773800761@qq.com>
2025-03-09 03:45:24 +0800

    Upload and update all files

--- kernel/ksud.h
+++ kernel/ksud.h
@@ -2,10 +2,19 @@
 #define __KSU_H_KSUD
 
 #include <linux/types.h>
+#include <linux/version.h>
+#define KERNEL_VERSION_5_10 KERNEL_VERSION(5, 10, 0)
+
+// Add Auto Add Symbol Export
+#if LINUX_VERSION_CODE < KERNEL_VERSION_5_10 || defined(CONFIG_KSU_HOOK)
+extern bool ksu_vfs_read_hook;
+extern bool ksu_execveat_hook;
+extern bool ksu_input_hook;
+#endif
 
 #define KSUD_PATH "/data/adb/ksud"
 
-void on_post_fs_data(void);
+void ksu_on_post_fs_data(void);
 
 bool ksu_is_safe_mode(void);
 
