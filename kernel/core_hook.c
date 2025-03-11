603dd05b3cce296158299dce9883225ad3774d0f
樱檩殇雪 <2773800761@qq.com>
2025-03-11 21:58:08 +0800

    Remove unnecessary hides

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -1103,12 +1103,8 @@ out_ksu_try_umount:
 	ksu_try_umount("/data/adb/modules", false, MNT_DETACH);
 	
 	// try umount hosts file
-	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH);
 	ksu_try_umount("/debug_ramdisk", false, MNT_DETACH);
-	ksu_try_umount("/sbin", false, MNT_DETACH);
 	
-	// try umount hosts file
-	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH);
 
 	// try umount lsposed dex2oat bins
 	ksu_try_umount("/apex/com.android.art/bin/dex2oat64", false, MNT_DETACH);
